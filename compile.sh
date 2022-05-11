6f6d938fd38f881d5d8b11587fc5df24ae1bdfc4
qiruizheng <4271439+qiruizheng@users.noreply.github.com>
2022-05-11 19:06:50 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
- #good for you
- version="21.02.3"
- wget https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
- xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
- tar -xvf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
- cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
+ #下面链接是最新的官方包路径，可以替换https://downloads.openwrt.org/releases/
+ wget https://downloads.openwrt.org/releases/22.03.0-rc1/targets/ramips/mt7620/openwrt-imagebuilder-22.03.0-rc1-ramips-mt7620.Linux-x86_64.tar.xz
+ xz -d openwrt-imagebuilder-22.03.0-rc1-ramips-mt7620.Linux-x86_64.tar.xz
+ tar -xvf openwrt-imagebuilder-22.03.0-rc1-ramips-mt7620.Linux-x86_64.tar
+ cd openwrt-imagebuilder-22.03.0-rc1-ramips-mt7620.Linux-x86_64
  make info
- make image PROFILE=phicomm_psg1218a PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap wpad-openssl -luci-app-shadowsocks-libev -shadowsocks-libev-ss-local -shadowsocks-libev-ss-rules -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
+ make image PROFILE=phicomm_K2 PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-mod-admin-full luci-theme-bootstrap wpad-openssl -luci-app-shadowsocks-libev -shadowsocks-libev-ss-local -shadowsocks-libev-ss-rules -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
